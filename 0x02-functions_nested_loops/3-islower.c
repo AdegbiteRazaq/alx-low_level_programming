323949a3162b159ffc5e095b0f3a63c50b47dd99
AdegbiteRazaq <adegbiterasaq101@gmail.com>
2023-09-12 13:51:53 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - checks for lowercase character.
+*@c: function paremeter.
+*
+*Return: 1 or 0.
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (!);
+	}
+	else
+	{
+		return (0);
+	}
+}

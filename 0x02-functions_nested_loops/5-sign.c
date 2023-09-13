da1a8544ecdc72a7e6bb83599f0c42c7345379e9
AdegbiteRazaq <adegbiterasaq101@gmail.com>
2023-09-12 18:04:09 -0700

    5sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
- * _abs - Print absolute value of a number
- * @a: function parameter
- * Return: Always 0.
+ * print_sign - Print the sign of a number
+ * @n: - function parameter
+ * Return: 1, 0, -1.
  */
-int _abs(int a)
+int print_sign(int n)
 {
-	int b = a * -1;
-
-	if (a >= 0)
+	if (n > 0)
 	{
-		return (a);
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
 	}
 	else
 	{
-		return (b);
+		_putchar(45);
+		return (-1);
 	}
-	return (0);
 }
